36c3157961fc0f61c5e32f4bc5c7909d601fb0f6
knsnyo <isc963@naver.com>
2023-06-30 01:19:57 +0900

    20230630 email auth finish

--- server/src/main.module.ts
+++ server/src/main.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import 'dotenv/config';
 import { PrismaService } from 'src/common/config/prisma.service';
 import { AuthModule } from 'src/modules/auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
