c4594b6fa1a596a151dc278601dced216e308225
pon06173 <pon06173@gmail.com>
2023-07-01 00:07:32 +0900

    2023년 07월 01일(회원가입창)

--- app/lib/src/modules/signup/view/signup.dart
+++ app/lib/src/modules/signup/view/signup.dart
@@ -1,55 +1,230 @@
 import 'package:flutter/material.dart';
 
 class Signup extends StatelessWidget {
-  const Signup({super.key});
+  const Signup({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    double fontSize = MediaQuery.of(context).size.width * 0.05;
+
     return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Container(
-                    width: 360,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Image.asset(
+                'assets/images/WarigariIcon.png',
+                width: 28,
+                height: 32,
+              ),
+              SizedBox(height: 10),
+              Text(
+                '회원가입',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '로그인 정보',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        '로그인',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: '6~16자/ 영문,소문자,숫자 사용 가능',
+                            hintStyle: TextStyle(fontSize: 14),
+                            border: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        '비밀번호',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: '8~18자/문자,숫자,특수 문자 모두 혼용',
+                            hintStyle: TextStyle(fontSize: 14),
+                            border: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                          obscureText: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        '비밀번호 확인',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: '비밀번호와 동일하게 작성',
+                            hintStyle: TextStyle(fontSize: 14),
+                            border: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                          obscureText: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 80),
+                  Text(
+                    '회원정보',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        '닉네임',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        '이메일',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: '이메일',
+                            hintStyle: TextStyle(fontSize: 14),
+                            border: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: double.infinity,
                     height: 50,
-                    color: Color(0x4472C4), // Replace with your desired color
+                    decoration: BoxDecoration(
+                      color: Color(0xFF4472C4),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
                     child: ElevatedButton(
                       onPressed: () {
                         debugPrint('.');
                       },
                       style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.all(16),
+                        primary: Colors.transparent,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
                       ),
                       child: Text(
-                        '회원가입',
+                        '가입하기',
+                        style: TextStyle(fontSize: 18),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).padding.top +
-                16, // Adjust the position based on your requirements
-            left: 12, // Adjust the position based on your requirements
-            child: IconButton(
-              icon: Icon(
-                Icons.arrow_back_ios_new_outlined,
-                color: Color(0xFF4472C4),
+                ],
               ),
-              onPressed: () {
-                Navigator.maybePop(context); // Handle the back button press
-              },
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
